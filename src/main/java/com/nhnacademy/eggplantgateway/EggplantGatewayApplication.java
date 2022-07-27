380f019c45e37ceb3929490005b6ac0ff3cd07df
조재철 <rhsnqk@gmail.com>
2022-07-27 16:52:45 +0900

    Create a Spring Boot Gateway project with Maven
    Co-authored-by: CalmDev <bunsung92@naver.com>

---
+++ src/main/java/com/nhnacademy/eggplantgateway/EggplantGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.eggplantgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggplantGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggplantGatewayApplication.class, args);
+	}
+
+}
